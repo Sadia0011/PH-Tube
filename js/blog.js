ad68b80ec3609ec513d23d817cd8de8f3628a8bc
sadia sultana <sadiamoon2019@gmail.com>
2023-08-31 23:11:07 +0600

    added the nav and tab section

---
+++ js/blog.js
@@ -0,0 +1,10 @@
+// const backBtn=document.getElementById('back-btn');
+const backBtnClick=()=>
+{
+    window.location.replace("../index.html");
+}
+
+
+const blogBtn=()=>{
+    window.location.replace('../blog.html');
+}
